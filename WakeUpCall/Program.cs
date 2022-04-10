bd4d3a957a2a241ec82ee64d2e6f9a5b2e24bf51
Julio <julio.oliveira178ads@gmail.com>
2022-04-10 02:33:19 -0300

    Adding 1 File

---
+++ WakeUpCall/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        float countPositives = arr.Count(x => x > 0);
+        float countZeroes = arr.Count(x => x == 0);
+        float countNegatives = arr.Count(x => x < 0);
+
+        
+
+        Console.WriteLine((countPositives / arr.Count()).ToString("0.000000"));
+        Console.WriteLine((countNegatives / arr.Count()).ToString("0.000000"));
+        Console.WriteLine((countZeroes / arr.Count()).ToString("0.000000"));
+        Console.ReadLine();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        /* int n = Convert.ToInt32(Console.ReadLine().Trim()); */
+
+        /* List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList(); */
+        var arr = "-4 3 -9 0 4 1".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        Result.plusMinus(arr);
+    }
+}
