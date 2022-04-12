38040400630690d640ddf98cb49baea94a67baa4
Julio <julio.oliveira178ads@gmail.com>
2022-04-12 13:41:44 -0300

    Last Changes

--- WakeUpCall/Program.cs
+++ WakeUpCall/Program.cs
@@ -13,14 +13,7 @@ using System.Text;
 using System;
 
 public class Result
-{
-
-    /*
-     * Complete the 'plusMinus' function below.
-     *
-     * The function accepts INTEGER_ARRAY arr as parameter.
-     */
-
+{    
     public static void PlusMinus(List<int> arr)
     {
         float countPositives = arr.Count(x => x > 0);
@@ -121,6 +114,22 @@ public class Result
         return Math.Abs(sumUpDown - sumDownUp);
 
     }
+
+    public static List<int> CountingSort(List<int> arr)
+    {
+        Dictionary<int, int> dictOut = new Dictionary<int, int>();
+        int i = 0;
+
+        foreach (var item in arr)
+        {
+            dictOut.Add(i, 0);
+            i++;
+        }
+
+        arr.ForEach(x => dictOut[x] += 1);
+        
+        return dictOut.Values.ToList();
+    }
 }
 
 class Solution
@@ -128,15 +137,10 @@ class Solution
     public static void Main(string[] args)
     {
 
-        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        int n = Convert.ToInt32("100".Trim());
 
-        List<List<int>> arr = new List<List<int>>();
+        List<int> arr = "63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        List<int> result = Result.CountingSort(arr);
 
-        for (int i = 0; i < n; i++)
-        {
-            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
-        }
-
-        int result = Result.DiagonalDifference(arr);
     }
 }
